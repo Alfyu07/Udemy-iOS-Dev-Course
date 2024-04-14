bfcec7913279faff060d527807365486b28b82a7
Wahyu Alfandi <w.alfa433@gmail.com>
2024-04-14 10:31:33 +0800

    fix: eggtimer app - bug when resetting timer and label

--- Projects/EggTimer/EggTimer/ViewController.swift
+++ Projects/EggTimer/EggTimer/ViewController.swift
@@ -12,37 +12,51 @@ import AVFoundation
 
 class ViewController: UIViewController {
      
-    
-    let eggTimes: Dictionary<String, Int> = [
-        "Soft" : 300,
-        "Medium" : 420,
-        "Hard" : 720
-    ]
-    
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
     var timer: Timer?
+    var secondTimer: Timer?
     var player: AVAudioPlayer?
-    
+
+    let eggTimes: Dictionary<String, Int> = [
+        "Soft" : 3,
+        "Medium" : 4,
+        "Hard" : 7
+    ]
     
     override func viewDidLoad() {
         progressView.progress = 0.0
     }
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        progressView.progress = 0.0
+        
+        player?.stop()
+        timer?.invalidate()
+        secondTimer?.invalidate()
+        
         let hardness = sender.currentTitle!
+        self.titleLabel.text = hardness
         activateTimer(seconds: eggTimes[hardness]!)
     }
     
     func activateTimer(seconds: Int){
         var remaining = seconds
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
-            if remaining > 0 {
-                self.progressView.progress = 1.0 - Float(remaining - 1) / Float(seconds)
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { timer in
+            if remaining >= 0 {
                 remaining -= 1
+                self.progressView.progress = 1.0 - Float(remaining) / Float(seconds)
                 self.playSound(soundName: "tick")
             } else{
                 timer.invalidate()
                 self.playSound(soundName: "alarm_sound")
+                self.titleLabel.text = "Done!"
+                
+                self.secondTimer = Timer.scheduledTimer(withTimeInterval: 4, repeats: false) { _ in
+                    self.titleLabel.text = "How do you like your eggs?"
+                    self.progressView.progress = 0.0
+                }
             }
         })
     }
